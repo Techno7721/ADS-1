38a9c16ff484fa17d7a14ebf6c65ed15031cbb39
Осинин Ярослав <99194733+Techno7721@users.noreply.github.com>
2022-02-14 19:13:14 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,96 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    if (value == 1) {
+        return false;
+    }
+    int brerak = 0;
+    for (uint64_t i = 2; i < value; ++i) {
+        if ((value % i) == 0) {
+            brerak = 1;
+            break;
+        }
+    }
+    if ((brerak == 0) || (value == 2)) {
+        return true;
+    }
+    if (brerak == 1) {
+        return false;
+    }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int brerak = 0;
+    if (n == 1) {
+        return 2;
+    }
+    if (n == 2) {
+        return 3;
+    }
+    uint64_t i = 3;
+    uint64_t count = 2;
+    while (true) {
+        i += 1;
+        for (uint64_t j = 2; j < i; ++j) {
+            if ((i % j) == 0) {
+                brerak = 1;
+                break;
+            }
+        }
+        if (brerak == 0) {
+            count += 1;
+            if (count == n) {
+                return i;
+            }
+        }
+        brerak = 0;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+ int brerak = 0;
+ uint64_t i = value;
+ while (true) {
+  i += 1;
+  for (uint64_t j = 2; j < i; ++j) {
+   if ((i % j) == 0) {
+    brerak = 1;
+    break;
+   }
+  }
+  if (brerak == 0) {
+   return i;
+  }
+  brerak = 0;
+ }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    if (hbound <= 2) {
+        return 0;
+    }
+    if (hbound == 3) {
+        return 2;
+    }
+    if (hbound == 4) {
+        return 5;
+    }
+    if (hbound == 5) {
+        return 5;
+    }
+    int brerak = 0;
+    uint64_t sum = 5;
+    for (uint64_t i = 5; i < hbound; ++i) {
+        for (uint64_t j = 2; j < i; ++j) {
+            if ((i % j) == 0) {
+                brerak = 1;
+                break;
+            }
+        }
+        if (brerak == 0) {
+            sum += i;
+        }
+        brerak = 0;
+    }
+    return sum;
 }
